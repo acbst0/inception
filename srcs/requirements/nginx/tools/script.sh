4096e04fc78499e352f5522f910c737bc2ebbe3a
acbst0 <ahmetcemilbostanoglu@yahoo.com>
2025-03-10 01:01:24 +0300

    Bu sefer oldu

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# Eğer sertifikalar yoksa oluştur
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+    echo "Self-signed sertifikalar oluşturuluyor..."
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/certs/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj "/C=TR/ST=Istanbul/L=Istanbul/O=Example/OU=IT/CN=abostano.42.fr"
+fi
+
 nginx -g "daemon off;"
