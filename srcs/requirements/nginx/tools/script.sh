dcaa5a39beb7da3237bcf2930eb9601fea2a6bac
acbst0 <ahmetcemilbostanoglu@yahoo.com>
2025-04-30 17:46:22 +0300

    Last fixes

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Eğer sertifikalar yoksa oluştur
-if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+if [ ! -f ${SSL_CERT_PATH} ]; then
     echo "Self-signed sertifikalar oluşturuluyor..."
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/certs/nginx-selfsigned.key \
-    -out /etc/ssl/certs/nginx-selfsigned.crt \
-    -subj "/C=TR/ST=Istanbul/L=Istanbul/O=Example/OU=IT/CN=abostano.42.fr"
+    -keyout ${SSL_KEY_PATH} \
+    -out ${SSL_CERT_PATH} \
+    -subj "/C=TR/ST=Istanbul/L=Istanbul/O=Example/OU=IT/CN=${DOMAIN}"
 fi
 
 exec nginx -g "daemon off;"
